13eaa7919eb5709f56958a3cb4f4e2042b3e7cf6
Daniel Vázquez <danlestal@gmail.com>
2016-03-15 19:26:39 +0100

    small converter from pdf to jpg. Not nearly happy with this

---
+++ OCRTests/PDFToImageConverterTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using OCR;
+
+namespace OCRTests
+{
+    [TestClass]
+    public class PDFToImageConverterTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void PDFToImageConverter_Read_Error_FileNotFound()
+        {
+            PDFToImageConverter.ConvertToImage("lol.pdf", "outputlol.png");
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"C:\programacion\PecanSoft\OCR-TPC\OCRTests\Resources\Cert.pdf")]
+        public void PDFToImageConverter_Read_Success()
+        {
+            PDFToImageConverter.ConvertToImage("Cert.pdf", "outputlol.png");
+            Assert.IsTrue(File.Exists("Cert.pdf"));
+        }
+    }
+}
