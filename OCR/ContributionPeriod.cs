cc1a7785af105a36b5633f42403f561cba8c83fa
Daniel Vázquez <danlestal@gmail.com>
2016-03-19 16:35:07 +0100

    uploading missing files

---
+++ OCR/ContributionPeriod.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace OCR
+{
+    public class ContributionPeriod
+    {
+
+        public ContributionPeriod(DateTime periodStart, DateTime periodEnd, decimal moneyContribution)
+        {
+            PeriodStart = periodStart;
+            PeriodEnd = periodEnd;
+            MoneyContribution = moneyContribution;
+        }
+
+        public DateTime PeriodStart { get; private set; }
+        public DateTime PeriodEnd { get; private set; }
+        public decimal MoneyContribution { get; private set; }
+        
+        /// <summary>
+        /// Converts the object into a Json string.
+        /// </summary>
+        /// <returns></returns>
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+    }
+}
