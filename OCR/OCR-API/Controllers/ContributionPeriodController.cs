87a02fae546daa7a7c517c7760a4b2522f16d7b9
Daniel Vázquez <danlestal@gmail.com>
2016-03-21 13:12:47 +0100

    starting the RESP api.

---
+++ OCR/OCR-API/Controllers/ContributionPeriodController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OCR_API.Controllers
+{
+    public class ContributionPeriodController : ApiController
+    {
+        // GET: api/ContributionPeriod
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/ContributionPeriod/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ContributionPeriod
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}
